16a0c9437ad2a7649d7cb866e4d13b733caca744
Pete Smith <peadar81@gmail.com>
2017-02-18 17:36:25 +0000

    Initial project

--- SpaceGame/GameViewController.swift
+++ SpaceGame/GameViewController.swift
@@ -48,7 +48,7 @@ class GameViewController: UIViewController {
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
+            return .portrait
         } else {
             return .all
         }
